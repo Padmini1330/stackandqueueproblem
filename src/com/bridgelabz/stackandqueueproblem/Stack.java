969fa7134e8d6d0fc14bb7c87e887768883cb7a3
padmini v <padmini.v@perfios.com>
2021-08-31 23:34:32 +0530

    ADDED: Stack class to perform push and display functions on stack

--- src/com/bridgelabz/stackandqueueproblem/Stack.java
+++ src/com/bridgelabz/stackandqueueproblem/Stack.java
@@ -1,9 +1,35 @@
 package com.bridgelabz.stackandqueueproblem;
 
-
-
-public class Stack 
+public class Stack<K> 
 {
+	private K key;
+	private INode head;
+	private INode tail;
+
+	public Stack() 
+	{
+		this.head = null;
+		this.tail = null;
+	}
+	public void push(INode newNode) 
+	{
+		if (tail == null)
+			this.tail = newNode;
+		if (head == null)
+			this.head = newNode;
+		else
+			newNode.setNext(head);
+		head = newNode;
+	}
 	
-	
+	public void printMyNodes() 
+	{
+		INode tempNode = head;
+		while (tempNode != null) 
+		{
+			System.out.print(tempNode.getKey() + "->");
+			tempNode = tempNode.getNext();
+		}
+		System.out.print("null");
+	}
 }
