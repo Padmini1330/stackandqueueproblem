14dec1b5d01b51975be456182cf0b2a394901602
padmini v <padmini.v@perfios.com>
2021-08-31 23:27:38 +0530

    ADDED: INode interface for node operations

--- src/com/bridgelabz/stackandqueueproblem/StackMain.java
+++ src/com/bridgelabz/stackandqueueproblem/StackMain.java
@@ -1,5 +1,19 @@
 package com.bridgelabz.stackandqueueproblem;
 
-public class StackMain {
+
+public class StackMain 
+{
+
+	public static void main(String[] args) 
+	{
+		Node<Integer> myfirstNode = new Node<>(70);
+		Node<Integer> mysecondNode = new Node<>(30);
+		Node<Integer> mythirdNode = new Node<>(56);
+		Stack<Integer> myStack = new Stack<Integer>();
+		myStack.push(myfirstNode);
+		myStack.push(mysecondNode);
+		myStack.push(mythirdNode);
+		myStack.printMyNodes();
+	}
 
 }
